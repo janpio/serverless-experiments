e25b14a5ccf6b8f8d2952e8c28a1a4178b43a007
Jan Piotrowski <piotrowski+github@gmail.com>
2021-04-18 19:54:54 +0200

    add sleep 1s

--- netlify/functions/invocation-count.js
+++ netlify/functions/invocation-count.js
@@ -1,11 +1,17 @@
 'use strict';
- 
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let invocation_count = 0
  
 exports.handler = async function(event, context) {
   invocation_count++
   console.log(invocation_count)
   
+  await sleep(1000)
+
   return {
     statusCode: 200,
     body: JSON.stringify({ invocation_count })
